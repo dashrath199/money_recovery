bb7b79074f85bad1287d58e8d260a9412861a22f
Dashrath <dashrath@bizaxl.com>
2026-07-24 13:30:39 +0530

    fix: remove disputed_amount column from aging bucket report - can't compute without sales_invoice

--- money_recovery/money_recovery/report/aging_bucket_summary/aging_bucket_summary.py
+++ money_recovery/money_recovery/report/aging_bucket_summary/aging_bucket_summary.py
@@ -20,7 +20,6 @@ def get_columns():
         {"label": _("61-90 Days"), "fieldname": "bucket_61_90", "fieldtype": "Currency", "width": 120},
         {"label": _("90+ Days"), "fieldname": "bucket_90_plus", "fieldtype": "Currency", "width": 120},
         {"label": _("Total Overdue"), "fieldname": "total_overdue", "fieldtype": "Currency", "width": 140},
-        {"label": _("Disputed"), "fieldname": "disputed_amount", "fieldtype": "Currency", "width": 120},
     ]
 
 
@@ -40,23 +39,12 @@ def get_data(filters):
     )
 
     customer_buckets = {}
-    customer_disputed = {}
     for inv in invoices:
         customer = inv.customer
         if customer not in customer_buckets:
             customer_buckets[customer] = {"0-30": 0, "31-60": 0, "61-90": 0, "90+": 0}
-            customer_disputed[customer] = 0
 
         days_overdue = date_diff(today, inv.due_date)
-
-        # Check if this invoice has a disputed Dunning record
-        disputed = frappe.db.get_value(
-            "Dunning",
-            {"dispute_flag": 1},
-            "name",
-        )
-        if disputed:
-            customer_disputed[customer] += inv.outstanding_amount
 
         if days_overdue <= 30:
             customer_buckets[customer]["0-30"] += inv.outstanding_amount
@@ -76,7 +64,6 @@ def get_data(filters):
             "bucket_61_90": buckets["61-90"],
             "bucket_90_plus": buckets["90+"],
             "total_overdue": total,
-            "disputed_amount": customer_disputed.get(customer, 0),
         })
 
     data.sort(key=lambda r: r["total_overdue"], reverse=True)
